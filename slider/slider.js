aa6e5f51f70abc4db3f964042485e2ad243c72bb
Volha Khainouskaya <volha.khainouskaya@gmail.com>
2021-12-22 21:59:16 +0300

    Слайдер: 2 cпособ динамич переменные

--- slider/slider.js
+++ slider/slider.js
@@ -1,22 +1,30 @@
 const leftBtn = document.querySelector("#left");
 const rightBtn = document.querySelector("#right");
-const items = document.querySelector("#items");
-const computedStyles = getComputedStyle(items);
+const itemsList = document.querySelector("#items");
+const computedStyles = getComputedStyle(itemsList);
+const items = document.querySelectorAll(".item");
+
+const minRight = 0;
+const itemWidth = getComputedStyle(items[0]).width;
+const step = parseInt(itemWidth);
+const preShowItems = 300 / step;
+const maxRight = (items.length - preShowItems) * step;
+let currentRight = 0;
+
+itemsList.style.right = currentRight;
 
 rightBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  let currentRight = parseInt(computedStyles.right);
-
-  if (currentRight < 500) {
-    items.style.right = `${currentRight + 100}px`;
+  if (currentRight < maxRight) {
+    currentRight += step;
+    itemsList.style.right = `${currentRight}px`;
   }
 });
 
 leftBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  let currentRight = parseInt(computedStyles.right);
-
-  if (currentRight > 0) {
-    items.style.right = `${currentRight - 100}px`;
+  if (currentRight > minRight) {
+    currentRight -= step;
+    itemsList.style.right = `${currentRight}px`;
   }
 });
