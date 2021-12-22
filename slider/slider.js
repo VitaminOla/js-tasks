f3031707da06ede3f1bc05409429c69fde84531c
Volha Khainouskaya <volha.khainouskaya@gmail.com>
2021-12-22 14:09:36 +0300

    Слайдер: RightBtn и LeftBtn 1 cпособ

--- slider/slider.js
+++ slider/slider.js
@@ -1,15 +1,24 @@
 const leftBtn = document.querySelector("#left");
 const rightBtn = document.querySelector("#right");
 const items = document.querySelector("#items");
-
-let currentRight = 0;
+const computedStyles = getComputedStyle(items);
 
 rightBtn.addEventListener("click", e => {
   e.preventDefault();
+  let currentRight = parseInt(computedStyles.right);
 
-  if (currentRight <500) {
-    currentRight += 100;
-    items.style.right = `${currentRight}px`;
+  if (currentRight < 500) {
+    items.style.right = `${currentRight + 100}px`;
   }
   
-})
+});
+
+leftBtn.addEventListener("click", e => {
+  e.preventDefault();
+  let currentRight = parseInt(computedStyles.right);
+
+  if (currentRight > 0) {
+    items.style.right = `${currentRight - 100}px`;
+  }
+  
+});
