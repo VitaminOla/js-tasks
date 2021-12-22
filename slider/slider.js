aa8bbc1d4eb128124f6cd0cae59d09a671c45a82
Volha Khainouskaya <volha.khainouskaya@gmail.com>
2021-12-22 14:43:36 +0300

    Слайдер: 1 cпособ

--- slider/slider.js
+++ slider/slider.js
@@ -3,22 +3,20 @@ const rightBtn = document.querySelector("#right");
 const items = document.querySelector("#items");
 const computedStyles = getComputedStyle(items);
 
-rightBtn.addEventListener("click", e => {
+rightBtn.addEventListener("click", (e) => {
   e.preventDefault();
   let currentRight = parseInt(computedStyles.right);
 
   if (currentRight < 500) {
     items.style.right = `${currentRight + 100}px`;
   }
-  
 });
 
-leftBtn.addEventListener("click", e => {
+leftBtn.addEventListener("click", (e) => {
   e.preventDefault();
   let currentRight = parseInt(computedStyles.right);
 
   if (currentRight > 0) {
     items.style.right = `${currentRight - 100}px`;
   }
-  
 });
